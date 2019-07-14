bf28e8876e78204a27a09638006cc53b1669f1b6
Raman Hrytsuk <Raman_Hrytsuk@epam.com>
2019-07-14 15:48:25 +0300

    2

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -19,27 +19,22 @@ namespace Web.Controllers
         {
             db = context;
         }
-        public async Task<IActionResult>
-            Index(string s, int page = 1, bool exist = true)
+        public async Task<IActionResult> Index(string s, int page = 1, bool exist = true)
         {
             IQueryable<Torrent> query = db.Torrents;
-            int count = 1000;
+            int count = await db.Torrents.CountAsync();
             int pageSize = 30;   // количество элементов на странице
             if (s == null && exist == true)
             {
-                count = await db.Torrents.CountAsync();
                 query = db.Torrents.Take(count).Where(t => t.Del == false).OrderByDescending(i => i.RegistredAt);
-
             }
             if (s == null && exist == false)
             {
-                count = await db.Torrents.CountAsync();
                 query = db.Torrents.Take(count).OrderByDescending(i => i.RegistredAt);
             }
             if (s != null)
             {
-                query = db.Torrents.Where(p => string.IsNullOrWhiteSpace(s) || EF.Functions.Like(p.Title, $"%{s}%"));
-                count = await query.CountAsync();
+                query = db.Torrents.Where(p => string.IsNullOrWhiteSpace(s) || EF.Functions.Like(p.Title, $"%{s}%")).OrderByDescending(i => i.RegistredAt);
             }
 
             var torents = await query.Skip((page - 1) * pageSize).Take(pageSize).ToArrayAsync();
@@ -62,3 +57,4 @@ namespace Web.Controllers
         }
     }
 }
+
