d5a8d058875dcbf0dc287c6a08295db2db6f4e80
Raman Hrytsuk <Raman_Hrytsuk@epam.com>
2019-07-25 15:23:38 +0300

    x

--- ApplicationCore/Entities/Torrent.cs
+++ ApplicationCore/Entities/Torrent.cs
@@ -9,7 +9,7 @@ namespace ApplicationCore.Entities
         public int Id { get; set; }
         public int TorrentId { get; set; }
         public DateTime RegistredAt { get; set; }
-        public string Size { get; set; }
+        public long Size { get; set; }
         public string Title { get; set; }
         public string Hash { get; set; }
         public int? TrackerId { get; set; }
