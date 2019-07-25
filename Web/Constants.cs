16c5978acde8419a1cbdd48757829c9565f00376
Raman Hrytsuk <Raman_Hrytsuk@epam.com>
2019-07-25 15:04:29 +0300

    324

---
+++ Web/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web
+{
+    public class Constants
+    {
+        public const int ITEMS_PER_PAGE = 10;
+        public const int FORUMS_PER_PAGE = 10;
+    }
+}
